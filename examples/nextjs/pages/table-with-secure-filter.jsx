c9768c76ceef4c98b9b3ca352a7b7f26063ea743
James Bowers <james@james-bowers.me>
2022-05-05 18:45:57 +0100

    line chart example

--- examples/nextjs/pages/table-with-secure-filter.jsx
+++ examples/nextjs/pages/table-with-secure-filter.jsx
@@ -4,6 +4,7 @@ export default () => (
   <VizzLib.Table
     componentId="cmp_1e60e61badfe44d9a159dcfb32153fb7"
     host="http://0.0.0.0:5000"
+    authEndpoint="/api/sign-my-filters"
     fields={["Price", "Region"]}
     filters={[]}
   />
