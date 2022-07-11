ec43a2784a70dd113eabe0265ab8f8d0c4c5b7f3
James Bowers <james@james-bowers.me>
2022-07-11 07:11:34 -0700

    update readme instructions

--- examples/nextjs/pages/line-chart.jsx
+++ examples/nextjs/pages/line-chart.jsx
@@ -2,7 +2,7 @@ import { ComponentContextProvider, LineChart } from "@vizzly/components";
 
 export default function Home() {
   return (
-    <div style={{height: "600px", width: "800px", padding: "3rem"}}>
+    <div style={{ height: "600px", width: "800px", padding: "3rem" }}>
       <ComponentContextProvider
         componentId="cmp_887a70a0b0b64935936cddc6edfc272e"
         apiConfig={{ host: "https://api.vizzly.co" }}
