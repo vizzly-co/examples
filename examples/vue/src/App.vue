b5e15ebc3b2e135eb62e24305238de90f7a659d4
James Bowers <james@vizzly.co>
2023-01-06 18:11:37 +0000

    a working view example

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,23 +1,70 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <DashboardBasic
+    type="in-browser"
+    :identityCallback="identityCallback"
+    :loadDataSetsCallback="loadDataSetsCallback"
+    :loadDataCallback="loadDataCallback"
+  />
 </template>
 
-<style scoped>
+<script lang="ts">
+import {applyReactInVue, applyPureReactInVue} from 'veaury'
+// This is a React component
+import Vizzly from '@vizzly/dashboard'
+import {ref} from 'vue'
+
+export default {
+  components: {
+    // Use HOC 'applyReactInVue' or 'applyPureReactInVue'
+    DashboardBasic: applyReactInVue(Vizzly.Dashboard),
+    DashboardBasicPure: applyPureReactInVue(Vizzly.Dashboard)
+  },
+  methods: {
+    loadDataSetsCallback: async (identityConfig: any) => {
+        const response = await fetch(`http://0.0.0.0:3005/api/resolve-data-sets`, {
+          method: "post",
+          body: JSON.stringify({ dataSets: identityConfig.dataSets }),
+        });
+
+        if (response.ok) {
+          const dataSets = await response.json();
+          return dataSets;
+        }
+      },
+      loadDataCallback: async (dataSet: any) => {
+          const response = await fetch(`http://0.0.0.0:3005/api/data/${dataSet.id}`);
+
+          if (response.ok) {
+            const result = await response.json();
+
+            return result;
+          } else {
+            console.error(`Failed to load data for data set ${dataSet.id}.`);
+
+            return null;
+          }
+        },
+        identityCallback: async () => {
+          const response = await fetch("http://0.0.0.0:3005/api/identity?type=custom");
+          if (response.ok) {
+            const tokens = await response.json();
+
+            return tokens;
+          }
+
+          return null;
+        }
+  }
+  // setup() {
+  //   return {}
+  // }
+}
+</script>
+
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
@@ -44,4 +91,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
