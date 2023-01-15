74217cf20b99139c550c8c858bece92ca97f3ac9
James Bowers <james@vizzly.co>
2023-01-15 22:16:43 +0000

    more comments

--- examples/next-js/pages/manage-dashboards.jsx
+++ examples/next-js/pages/manage-dashboards.jsx
@@ -45,17 +45,27 @@ const ManageDashboards = () => {
       <button
         className="py-1 px-2 shadow-sm bg-blue-500 text-white rounded-sm mt-3"
         onClick={async () => {
+          // Create a "basic table" component and provide the data set for it to use.
           let basicTable = VizzlyDashboardSDK.BasicTable.init(
             "das_69948d88edaf4e55a864a0f25357139f"
           );
+
+          // Set the fields on the table by specifying a list of IDs from the data set
+          // defined in your Vizzly config file.
           basicTable = VizzlyDashboardSDK.BasicTable.setFields(basicTable, [
             "User Name",
             "Region",
             "Customer Since",
           ]);
 
+          // Initialize an empty dashboard that we'll use to override the template.
           let dashboard = VizzlyDashboardSDK.emptyDashboard();
+
+          // Add a row to the dashboard
           dashboard = VizzlyDashboardSDK.insertRow(dashboard, FIRST_ROW);
+
+          // Add the basic table component to the first row, first cell of the
+          // dashboard.
           dashboard = VizzlyDashboardSDK.setComponentOnDisplay(
             dashboard,
             basicTable,
@@ -63,8 +73,8 @@ const ManageDashboards = () => {
             FIRST_CELL
           );
 
-          // Create a copy of a template, and override the dashboard itself
-          // and the metadata.
+          // Call the Vizzly API to create a copy of the template, but with the
+          // dashboard and metadata overrides.
           const created = await createTemplateCopy({
             dashboard,
             metadata: { anyKeyValue: "that you want" },
