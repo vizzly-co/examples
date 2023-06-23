244b5ae16d27da85ad28ab4e755a5cc9c0bd12fb
Chris Palmer <palmer.minto@gmail.com>
2023-06-23 08:58:57 +0100

    VIZZ-532 document icons

---
+++ storybook/storybook-react/src/stories/render/LibraryIcon.stories.tsx
@@ -0,0 +1,62 @@
+import Vizzly from '@vizzly/dashboard';
+import type { Meta, StoryFn } from '@storybook/react';
+
+import { waitForElement } from '../testing/helpers';
+import { getIdentity } from '../factory/getIdentity';
+import { screenUpdate } from '../testing/utils';
+import { highlight } from '../testing/highlighter';
+
+const meta: Meta<typeof Vizzly.Dashboard> = {
+  title: 'Dashboard Props/render/LibraryIcon',
+  component: Vizzly.Dashboard,
+};
+
+export default meta;
+
+export const Example: StoryFn = () => {
+  return (
+    <Vizzly.Dashboard
+      theme={{
+        detail: 'minimal',
+        rowLimit: 2,
+      }}
+      renderLibraryIcon={() => (
+        <div data-component="renderLibraryIcon">library</div>
+      )}
+      queryEngineEndpoint="https://example.vizzly.co/query-engine"
+      identity={getIdentity()}
+    />
+  );
+};
+
+Example.play = async () => {
+  waitForElement('.vizzly_dashboard', async () => {
+    await screenUpdate();
+
+    document.body
+      .querySelectorAll('[data-component="renderLibraryIcon"]')
+      .forEach((element) => highlight(element as HTMLElement));
+  });
+};
+
+export const Original: StoryFn = () => {
+  return (
+    <Vizzly.Dashboard
+      theme={{
+        detail: 'minimal',
+        rowLimit: 2,
+      }}
+      queryEngineEndpoint="https://example.vizzly.co/query-engine"
+      identity={getIdentity()}
+    />
+  );
+};
+
+Original.play = async () => {
+  waitForElement('.vizzly_dashboard', async () => {
+    await screenUpdate();
+    document.body
+      .querySelectorAll('[data-component="select-template"]')
+      .forEach((element) => highlight(element as HTMLElement));
+  });
+};
