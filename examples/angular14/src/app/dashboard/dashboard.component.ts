1ac26083884a6272ba636c1a4483ba55429e1149
James Bowers <james@vizzly.co>
2023-08-22 17:14:07 +0100

    add helpful get started comments

--- examples/angular14/src/app/dashboard/dashboard.component.ts
+++ examples/angular14/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
-import "@vizzly/dashboard-webcomponent";
+// @ts-nocheck
 
-// @ts-ignore
+import "@vizzly/dashboard-webcomponent";
 import type { VizzlyDashboard } from "@vizzly/dashboard-webcomponent";
 import { Component, ElementRef, AfterViewInit, ViewChild, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
  
@@ -13,22 +13,30 @@ import { Component, ElementRef, AfterViewInit, ViewChild, CUSTOM_ELEMENTS_SCHEMA
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
-// @ts-ignore
 export class VizzlyDashboard implements AfterViewInit {
-  // @ts-ignore
   @ViewChild("dashboard") dashboardRef: ElementRef<VizzlyDashboard>;
- 
+
   ngAfterViewInit() {
     this.dashboardRef.nativeElement.render({
       // Pass your props here
-      queryEngineEndpoint: 'http://0.0.0.0:8000',
-      vizzlyApiHost: 'http://0.0.0.0:7080',
+      queryEngineEndpoint: '<< Your query engine endpoint - e.g http://0.0.0.0:8000 >>',
       identity: async () => {
+
+        /*
+        Implement the Vizzly identity logic, or use the CLI to generate test access tokens.
+
+        https://docs.vizzly.co/cli#access-tokens
+        https://docs.vizzly.co/dashboard/properties/identity
+        https://docs.vizzly.co/concepts/identity-config
+
         return {
-          "dashboardAccessToken": "eyJhbGciOiJFUzI1NiIsImtpZCI6Ijh5eFpjeUQzRTZBWm1TYUVSWi1kblE4TnQtbTJ2LWhJQktrbTJ2eDQyd0kifQ.eyJhY2Nlc3NUeXBlIjoic3RhbmRhcmQiLCJvcmdhbmlzYXRpb25JZCI6Im9yZ183ZTU4ZjU2Y2VmZjg0ZjgwYmM1MjliNTdmODAyZDYzOCIsInVzZXJSZWZlcmVuY2UiOiJ1c2VyX2VkaXRvciAtIHNlbGYtaG9zdGVkIiwiZXhwaXJlcyI6IjIwMjMtMDgtMjJUMTc6NTc6MDcuMzYzWiJ9.DTo-FCZ-spbT4eT9Ux4pj0gy-Xyz8O_x7xxt44RiQtEL8rzL82jzxIGDYelmRx-TGSsA2b684aDVNi54e-i-2g",
-          "dataAccessToken": "eyJhbGciOiJFUzI1NiIsImtpZCI6Ijh5eFpjeUQzRTZBWm1TYUVSWi1kblE4TnQtbTJ2LWhJQktrbTJ2eDQyd0kifQ.eyJkYXRhU2V0SWRzIjoiKiIsInNlY3VyZUZpbHRlcnMiOnsic2YiOlt7ImZpZWxkIjoic2Rmc2Rmc2RmIiwib3AiOiI9IiwidmFsdWUiOjN9XX0sImV4cGlyZXMiOiIyMDIzLTA4LTIyVDE3OjU3OjA3LjM1M1oifQ.QXEaqycRm8gxrBh1byF0SZlRoMhIVjIjZFsXEnQbJQrtzlUQS1y9om6VFzIbbkOB778k8yGSuC3d7kdkTtKiiQ",
-          "queryEngineAccessToken": "eyJhbGciOiJFUzI1NiIsImtpZCI6Ijh5eFpjeUQzRTZBWm1TYUVSWi1kblE4TnQtbTJ2LWhJQktrbTJ2eDQyd0kifQ.eyJhbGxvd0RhdGFiYXNlU2NoZW1hQWNjZXNzIjp0cnVlLCJhbGxvd0RhdGFQcmV2aWV3QWNjZXNzIjpmYWxzZSwiZXhwaXJlcyI6IjIwMjMtMDgtMjJUMTc6NTc6MDcuMzY0WiJ9.nnIeyNOAw35epVj0vT1fHneim9oyTUxqHsnf4ThhKt55R5uCNw3jsCptAeGbkH3kygJJ1AxZRoL0Xh0AE18sYQ"
-        };
+            "dashboardAccessToken": "...",
+            "dataAccessToken": "...",
+            "queryEngineAccessToken": "..."
+          };
+        */
+
+        return null;
       }
     });
   }
