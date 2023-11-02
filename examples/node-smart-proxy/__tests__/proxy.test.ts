f855b1f217b3fb2d17eb0fcc0fb06946bba2f837
James Bowers <james@vizzly.co>
2023-11-02 10:17:55 +0000

    test resolving data sets through the proxy

--- examples/node-smart-proxy/__tests__/proxy.test.ts
+++ examples/node-smart-proxy/__tests__/proxy.test.ts
@@ -1,52 +1,133 @@
-import supertest from 'supertest';
+import supertest from "supertest";
+import { createSigner } from "@vizzly/auth";
+import * as Config from "../src/Config";
 
-describe('Proxy', () => {
-  test('status endpoint', async () => {
-    const { app } = require('../src/index.ts');
+const signDataAccessToken = async (): Promise<string> => {
+  const signer = createSigner({
+    ttlInMinutes: 1,
+    privateKey: Config.getVizzlyPrivateKey(),
+  });
+
+  return await signer.signDataAccessToken({
+    projectId: "prj_3e37665d7a5b40a9b9e8a8ad7128e738",
+    dataSetIds: "*",
+    secureFilters: {},
+  });
+};
+
+describe("Proxy", () => {
+  test("status endpoint - API", async () => {
+    const { app } = require("../src/index.ts");
 
     const response = await supertest(app)
-      .post('/api/status')
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
+      .post("/api/status")
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
       .send({});
 
     expect(response.status).toEqual(200);
-    expect(response.headers).toHaveProperty('response-time');
-    expect(response.headers).toHaveProperty('vizzly-query-engine-request-id');
-    expect(response.headers['vizzly-query-engine-strategy']).toEqual('dynamic');
-    expect(response.headers['content-type']).toEqual(expect.stringContaining('application/json'));
+    expect(response.headers).toHaveProperty("response-time");
+    expect(response.headers).toHaveProperty("vizzly-query-engine-request-id");
+    expect(response.headers["vizzly-query-engine-strategy"]).toEqual("dynamic");
+    expect(response.headers["content-type"]).toEqual(
+      expect.stringContaining("application/json"),
+    );
 
     expect(response.body).toEqual({
-       "allowGetStarted": null,
-       "client": "postgres",
-       "configFound": true,
-       "configSources": {
-         "connection": "environment",
-         "dataSets": [],
-         "saveLocations": [
-           "file",
-           "vizzlyEncrypted",
-         ],
-       },
-       "configValid": true,
-       "connectionOk": true,
-       "dataSetsCount": 1,
-       "message": "Ready to accept queries",
-       "version": expect.any(String),
-    })
+      allowGetStarted: null,
+      client: "postgres",
+      configFound: true,
+      configSources: {
+        connection: "environment",
+        dataSets: [],
+        saveLocations: ["file", "vizzlyEncrypted"],
+      },
+      configValid: true,
+      connectionOk: true,
+      dataSetsCount: 1,
+      message: "Ready to accept queries",
+      version: expect.any(String),
+    });
+  });
+
+  describe("resolve data sets", () => {
+    test("for user 1", async () => {
+      const { app } = require("../src/index.ts");
+
+      const response = await supertest(app)
+        .post("/api/v1/resolve-data-sets")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .set("Cookie", "user=1")
+        .set("Auth", await signDataAccessToken())
+        .send({});
+
+      expect(response.status).toEqual(200);
+      expect(response.headers).toHaveProperty("response-time");
+      expect(response.headers).toHaveProperty("vizzly-query-engine-request-id");
+      expect(response.headers["vizzly-query-engine-strategy"]).toEqual(
+        "dynamic",
+      );
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("application/json"),
+      );
+
+      expect(response.body).toEqual({
+        dataSets: [
+          {
+            id: "das_dynamic",
+            name: "A dynamic data set for user 1!",
+            fields: [],
+          },
+        ],
+      });
+    });
+
+    test("for user 2", async () => {
+      const { app } = require("../src/index.ts");
+
+      const response = await supertest(app)
+        .post("/api/v1/resolve-data-sets")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .set("Cookie", "user=2")
+        .set("Auth", await signDataAccessToken())
+        .send({});
+
+      expect(response.status).toEqual(200);
+      expect(response.headers).toHaveProperty("response-time");
+      expect(response.headers).toHaveProperty("vizzly-query-engine-request-id");
+      expect(response.headers["vizzly-query-engine-strategy"]).toEqual(
+        "dynamic",
+      );
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("application/json"),
+      );
+
+      expect(response.body).toEqual({
+        dataSets: [
+          {
+            id: "das_dynamic",
+            name: "A dynamic data set for user 2!",
+            fields: [
+              {
+                id: "fie_1",
+                publicName: "Home Country",
+                dataType: "string",
+                allowedGranularities: expect.any(Array),
+                allowedOperators: expect.any(Array),
+              },
+              {
+                id: "fie_2",
+                publicName: "Price in $",
+                dataType: "number",
+                allowedGranularities: expect.any(Array),
+                allowedOperators: expect.any(Array),
+              },
+            ],
+          },
+        ],
+      });
+    });
   });
 });
-
-
-
-// const databaseConnectionCredentials = await encrypt<DbConnectionContent>({
-//   expires: addMinutes(new Date(), 1).toISOString(),
-//   credentials: {
-//     password: 'customer',
-//     database: 'my_database',
-//     user: 'customer',
-//     host: '0.0.0.0',
-//     port: 7405,
-//   },
-// });
-
