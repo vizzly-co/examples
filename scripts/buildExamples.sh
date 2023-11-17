0a2cfbf911996f1c1c08a536e7cafcabc93041a1
Chris Palmer <palmer.minto@gmail.com>
2023-11-17 09:04:34 +0000

    VIZZ--691 make example work with build

--- scripts/buildExamples.sh
+++ scripts/buildExamples.sh
@@ -2,11 +2,8 @@
 
 cd ./examples/angular-16
 
-# Install the latest webcomponent package
-npm install @vizzly/dashboard-webcomponent@latest --legacy-peer-deps
-
 # Build the angular example
 npm run build -- --configuration production
 
 # Copy the output to the static builds folder
-cp -r ./dist/angular-16 ../../static-build-outputs
+cp -r ./dist/angular16 ../../static-build-outputs
